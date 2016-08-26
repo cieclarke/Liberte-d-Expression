6f1678d299075a81540cfffc40e65f622ab57dc1
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2016-08-26 12:52:49 +0100

    Experimenting with the config.php language meta info... I think this is overridden by Yii somewhere.

--- liberte-web/tests/codeception/config/config.php
+++ liberte-web/tests/codeception/config/config.php
@@ -3,7 +3,7 @@
  * Application configuration shared by all test types
  */
 return [
-    'language' => 'en-US',
+    'language' => 'en',
     'controllerMap' => [
         'fixture' => [
             'class' => 'yii\faker\FixtureController',
