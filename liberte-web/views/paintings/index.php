83927a313bbac9f9f17d2a6bf751e724573d99a3
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2016-11-04 16:07:04 +0000

    Lining up and colouring in - paintings page.

--- liberte-web/views/paintings/index.php
+++ liberte-web/views/paintings/index.php
@@ -21,12 +21,18 @@
         <img src="/images/paintings_in-situ_dance-with-me.jpg" alt="Dance with me" />
     </li>
     <li class="container-lozenge in-situ-06">
-    
+        <img src="/images/paintings_in-situ_collage_00.jpg" alt="Collage of paintings" />
     </li>
     <li class="container-lozenge in-situ-07">
-
+        <h2>Artwork Exhibition</h2>
+        <p>Liberte d’Expression has organised art exhibitions where the aim of these events is to give painters across the UK exposure for their art irrespective of their age, experience or artistic background whilst raising funding for really good causes.</p>
+        <ul class="list-standard">
+            <li>
+                <a href="#">Barclays Bank and Candid Arts Trust</a>
+            </li>
+        </ul>
     </li>
     <li class="container-lozenge in-situ-08">
-    
+        <img src="/images/brick_wall_white_logo_00.jpg" alt="Liberte d’Expression" />
     </li>
 </ul>
