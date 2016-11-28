367cbc074144322e137f17acf780077b8c47effc
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2016-11-28 00:52:11 +0000

    Add the latest design changes to paintings page only

--- liberte-web/views/paintings/index.php
+++ liberte-web/views/paintings/index.php
@@ -2,37 +2,63 @@
     $this->title = 'Paintings';
 ?>
 
-<h1 class="off-screen">Paintings</h1>
+<section class="choice">
+    <h1>Artwork Exhibition</h1>
+    <p>Liberte d’Expression has organised art exhibitions where the aim of these events is to give painters across the UK exposure for their art irrespective of their age, experience or artistic background whilst raising funding for really good causes.</p>
 
-<ul class="paintings-gallery">
-    <li class="container-lozenge in-situ-02">
-        <img src="/images/paintings_in-situ_la-legante.jpg" alt="La legante" />
-    </li>
-    <li class="container-lozenge in-situ-02">
-        <img src="/images/paintings_in-situ_winter-mist.jpg" alt="Winter Mist" />
-    </li>
-    <li class="container-lozenge in-situ-03">
-        <img src="/images/paintings_in-situ_seizing-opportunities.jpg" alt="Seizing opportunities" />
-    </li>
-    <li class="container-lozenge in-situ-04">
-        <img src="/images/paintings_in-situ_eden.jpg" alt="Eden" />
-    </li>
-    <li class="container-lozenge in-situ-05">
-        <img src="/images/paintings_in-situ_dance-with-me.jpg" alt="Dance with me" />
-    </li>
-    <li class="container-lozenge in-situ-06">
-        <img src="/images/paintings_in-situ_collage_00.jpg" alt="Collage of paintings" />
-    </li>
-    <li class="container-lozenge in-situ-07">
-        <h2>Artwork Exhibition</h2>
-        <p>Liberte d’Expression has organised art exhibitions where the aim of these events is to give painters across the UK exposure for their art irrespective of their age, experience or artistic background whilst raising funding for really good causes.</p>
-        <ul class="list-standard">
-            <li>
-                <a href="#">Barclays Bank and Candid Arts Trust</a>
-            </li>
-        </ul>
-    </li>
-    <li class="container-lozenge in-situ-08">
-        <img src="/images/brick_wall_white_logo_00.jpg" alt="Liberte d’Expression" />
-    </li>
-</ul>
+    <ul class="gallery">
+        <li>
+            <h2>Barclays</h2>
+            <a href="#barclays" class="container-lozenge">
+                <img src="/images/paintings_in-situ_la-legante.jpg" alt="La legante" />
+            </a>
+        </li>
+        <li>
+            <h2>Light Gallery</h2>
+            <a href="#light" class="container-lozenge">
+                <img src="/images/paintings_in-situ_volcano.jpg" alt="Volcano" />
+            </a>
+        </li>
+        <li>
+            <h2>Torbay Hospital</h2>
+            <a href="#torbay" class="container-lozenge">
+                <img src="/images/paintings_in-situ_winter-mist.jpg" alt="Winter Mist" />
+            </a>
+        </li>
+    </ul>
+</section>
+
+
+<section class="detail container-lozenge">
+    <header>
+        <h2 class="off-screen">Exhibition details</h2>
+
+        <h3 id="barclays">Barclays Bank and Candid Arts Trust</h3>
+    </header>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mattis vitae nisi eget eleifend. Phasellus tristique ut augue a consequat. Sed at dignissim augue, et tempor enim. Nunc viverra magna ut ex pretium, ac tincidunt ante tempus. Vestibulum non purus id augue fringilla varius vitae eu dolor. 
+    </p>
+    <p>
+        Proin lectus metus, facilisis nec mollis non, egestas convallis ante. Vestibulum nec nisi malesuada, sodales massa nec, dictum nisi. Sed id magna suscipit, vestibulum magna id, ultricies nisi. Etiam eu aliquet purus. Nullam eget augue interdum tortor semper finibus eget a urna. Suspendisse pretium justo non erat semper, nec vulputate velit egestas. Nam vitae sollicitudin tortor, id porta justo. Duis sagittis purus nec consectetur euismod.
+    </p>
+
+    <h3 id="light">Light Gallery</h3>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mattis vitae nisi eget eleifend. Phasellus tristique ut augue a consequat. Sed at dignissim augue, et tempor enim. Nunc viverra magna ut ex pretium, ac tincidunt ante tempus. Vestibulum non purus id augue fringilla varius vitae eu dolor. 
+    </p>
+    <p>
+        Proin lectus metus, facilisis nec mollis non, egestas convallis ante. Vestibulum nec nisi malesuada, sodales massa nec, dictum nisi. Sed id magna suscipit, vestibulum magna id, ultricies nisi. Etiam eu aliquet purus. Nullam eget augue interdum tortor semper finibus eget a urna. Suspendisse pretium justo non erat semper, nec vulputate velit egestas. Nam vitae sollicitudin tortor, id porta justo. Duis sagittis purus nec consectetur euismod.
+    </p>
+
+    <h3 id="torbay">Torbay Hospital</h3>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mattis vitae nisi eget eleifend. Phasellus tristique ut augue a consequat. Sed at dignissim augue, et tempor enim. Nunc viverra magna ut ex pretium, ac tincidunt ante tempus. Vestibulum non purus id augue fringilla varius vitae eu dolor. 
+    </p>
+    <p>
+        Proin lectus metus, facilisis nec mollis non, egestas convallis ante. Vestibulum nec nisi malesuada, sodales massa nec, dictum nisi. Sed id magna suscipit, vestibulum magna id, ultricies nisi. Etiam eu aliquet purus. Nullam eget augue interdum tortor semper finibus eget a urna. Suspendisse pretium justo non erat semper, nec vulputate velit egestas. Nam vitae sollicitudin tortor, id porta justo. Duis sagittis purus nec consectetur euismod.
+    </p>
+
+</section>
