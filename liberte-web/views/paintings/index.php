d85569dfce978202867add387dc718c47833b97d
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-02-20 21:51:34 +0000

    Amalgamate four exhibition galleries into two

--- liberte-web/views/paintings/index.php
+++ liberte-web/views/paintings/index.php
@@ -6,10 +6,15 @@
 <p>Liberte d'Expression has organised art exhibitions where the aim of these events is to give painters across the UK exposure for their art irrespective of their age, experience or artistic background whilst raising funding for really good causes.</p>
 
 <section class="barclays">
-    <h2>Barclays Bank &amp; Candid Arts Trust</h2>
+    <h2>Barclays Bank, Candid Art Trust and Light Gallery</h2>
     <figure class="container-lozenge image image-embed">
         <img src="/images/exhibitions-paintings/exhibition_barclays_00.jpg" alt="Exhibition scene - Barclays" />
     </figure>
+    <!--
+    <figure class="container-lozenge image image-embed">
+        <img src="/images/exhibitions-paintings/exhibition_light-gallery_00.jpg" alt="Exhibition scene - Light Gallery" />
+    </figure>
+    -->
     <p>
         With the support of Barclays Bank, Communisis and Candid Arts Trust, Liberté d'Expression launched in 2006 a contemporary art competition. 
     </p>
@@ -18,6 +23,9 @@
     </p>
     <p>
         As part of the exhibition, the general public was asked to vote for their favourite paintings.
+    </p>
+    <p>
+        A total of twenty-five artists were showcased in the Light Gallery; in abstract, still life, figurative or landscape subjects, there was something for all tastes. 
     </p>
     <ul class="gallery">
         <li>
@@ -65,18 +73,6 @@
                 <img src="/images/exhibitions-paintings/barclays/PB020155_thumb.jpg" alt="A Barclays Bank and Candid Arts Trust exhibition scene" />
             </a>
         </li>
-    </ul>
-</section>
-
-<section class="light">
-    <h2>Light Gallery</h2>
-    <figure class="container-lozenge image image-embed">
-        <img src="/images/exhibitions-paintings/exhibition_light-gallery_00.jpg" alt="Exhibition scene - Light Gallery" />
-    </figure>
-    <p>
-        A total of twenty-five artists were showcased; in abstract, still life, figurative or landscape subjects, there was something for all tastes. 
-    </p>
-    <ul class="gallery">
         <li>
             <a href="/images/exhibitions-paintings/light_gallery/Dscn1492.jpg">
                 <img src="/images/exhibitions-paintings/light_gallery/Dscn1492_thumb.jpg" alt="A Light Gallery exhibition scene" />
@@ -90,60 +86,21 @@
     </ul>
 </section>
 
-<section class="torbay">
-    <h2>Torbay Hospital</h2>
-    <figure class="container-lozenge image image-embed">
-        <img src="/images/exhibitions-paintings/exhibition_torbay_00.jpg" alt="Exhibition scene - Torbay" />
-    </figure>
-    <p>
-        The Gallery is funded through a charitable trust – South Devon Healthcare Arts which promotes Arts in Hospitals. 
-    </p>
-    <ul class="gallery">
-        <li>
-            <a href="/images/exhibitions-paintings/torbay/P2290687.jpg">
-                <img src="/images/exhibitions-paintings/torbay/P2290687_thumb.jpg" alt="A Torbay exhibition scene" />
-            </a>
-        </li>
-        <li>
-            <a href="/images/exhibitions-paintings/torbay/P2290694.jpg">
-                <img src="/images/exhibitions-paintings/torbay/P2290694_thumb.jpg" alt="A Torbay exhibition scene" />
-            </a>
-        </li>
-        <li>
-            <a href="/images/exhibitions-paintings/torbay/P2290698.jpg">
-                <img src="/images/exhibitions-paintings/torbay/P2290698_thumb.jpg" alt="A Torbay exhibition scene" />
-            </a>
-        </li>
-        <li>
-            <a href="/images/exhibitions-paintings/torbay/P2290701.jpg">
-                <img src="/images/exhibitions-paintings/torbay/P2290701_thumb.jpg" alt="A Torbay exhibition scene" />
-            </a>
-        </li>
-        <li>
-            <a href="/images/exhibitions-paintings/torbay/P2290702.jpg">
-                <img src="/images/exhibitions-paintings/torbay/P2290702_thumb.jpg" alt="A Torbay exhibition scene" />
-            </a>
-        </li>
-        <li>
-            <a href="/images/exhibitions-paintings/torbay/P2290706.jpg">
-                <img src="/images/exhibitions-paintings/torbay/P2290706_thumb.jpg" alt="A Torbay exhibition scene" />
-            </a>
-        </li>
-        <li>
-            <a rel="group" href="/images/exhibitions-paintings/torbay/P2290707.jpg">
-                <img src="/images/exhibitions-paintings/torbay/P2290707_thumb.jpg" alt="A Torbay exhibition scene" />
-            </a>
-        </li>
-    </ul>
-</section>
-
 <section class="sharmina">
     <h2>Sharmina Karim</h2>
     <figure class="container-lozenge image image-embed">
         <img src="/images/exhibitions-paintings/exhibition_sharmina_00.jpg" alt="Exhibition scene - Sharmina" />
     </figure>
+    <!--
+    <figure class="container-lozenge image image-embed">
+        <img src="/images/exhibitions-paintings/exhibition_torbay_00.jpg" alt="Exhibition scene - Torbay" />
+    </figure>
+    -->
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mattis vitae nisi eget eleifend. Phasellus tristique ut augue a consequat. Sed at dignissim augue, et tempor enim. Nunc viverra magna ut ex pretium, ac tincidunt ante tempus. Vestibulum non purus id augue fringilla varius vitae eu dolor. 
+    </p>
+    <p>
+        The Torbay Hospital Gallery is funded through a charitable trust – South Devon Healthcare Arts which promotes Arts in Hospitals. 
     </p>
     <ul class="gallery">
         <li>
@@ -231,6 +188,41 @@
         <li>
             <a href="/images/exhibitions-paintings/sharmina/14.jpg">
                 <img src="/images/exhibitions-paintings/sharmina/14_thumb.jpg" alt="A Sharmina exhibit" />
+            </a>
+        </li>
+        <li>
+            <a href="/images/exhibitions-paintings/torbay/P2290687.jpg">
+                <img src="/images/exhibitions-paintings/torbay/P2290687_thumb.jpg" alt="A Torbay exhibition scene" />
+            </a>
+        </li>
+        <li>
+            <a href="/images/exhibitions-paintings/torbay/P2290694.jpg">
+                <img src="/images/exhibitions-paintings/torbay/P2290694_thumb.jpg" alt="A Torbay exhibition scene" />
+            </a>
+        </li>
+        <li>
+            <a href="/images/exhibitions-paintings/torbay/P2290698.jpg">
+                <img src="/images/exhibitions-paintings/torbay/P2290698_thumb.jpg" alt="A Torbay exhibition scene" />
+            </a>
+        </li>
+        <li>
+            <a href="/images/exhibitions-paintings/torbay/P2290701.jpg">
+                <img src="/images/exhibitions-paintings/torbay/P2290701_thumb.jpg" alt="A Torbay exhibition scene" />
+            </a>
+        </li>
+        <li>
+            <a href="/images/exhibitions-paintings/torbay/P2290702.jpg">
+                <img src="/images/exhibitions-paintings/torbay/P2290702_thumb.jpg" alt="A Torbay exhibition scene" />
+            </a>
+        </li>
+        <li>
+            <a href="/images/exhibitions-paintings/torbay/P2290706.jpg">
+                <img src="/images/exhibitions-paintings/torbay/P2290706_thumb.jpg" alt="A Torbay exhibition scene" />
+            </a>
+        </li>
+        <li>
+            <a rel="group" href="/images/exhibitions-paintings/torbay/P2290707.jpg">
+                <img src="/images/exhibitions-paintings/torbay/P2290707_thumb.jpg" alt="A Torbay exhibition scene" />
             </a>
         </li>
     </ul>
