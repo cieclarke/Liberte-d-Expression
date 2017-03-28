971e0d80055102a228c6b2d71032cf570db4dddf
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-03-28 16:39:04 +0100

    Add initial layout and appearance of 'about' page

--- liberte-web/views/about/index.php
+++ liberte-web/views/about/index.php
@@ -4,38 +4,40 @@
 
 <h1>About the team</h1>
 
-<article>
-    <h2>Natalie Szefer</h2>
-    <p>Natalie is a Yorkshire based graphic designer with over 10 years experience working with various types of clients.</p>
-    <p>As well as computer based design, she also love lino carving and using other handmade methods to create different types of artwork.</p>
-</article>
+<div class="flex-block">
+    <article>
+        <h2>Natalie Szefer</h2>
+        <p>Natalie is a Yorkshire based graphic designer with over 10 years experience working with various types of clients.</p>
+        <p>As well as computer based design, she also love lino carving and using other handmade methods to create different types of artwork.</p>
+    </article>
 
-<article>
-    <h2>Adebanji Alade</h2>
-    <p>Banji started off drawing when he was six years old, something he has always loved to do. This passion to sketch and draw has blossomed into a full professional career as a painter of people and places. He is now a member of the Royal Institute of Oil Painters, The Chelsea Art Society and The Guild of Fine Art, Nigeria. He has made many appearances on TV, sketching and painting portraits of famous people on The One Show, BBC One. He is also a London Correspondent with Urban Sketchers Worldwide. Adebanji teaches Painting and Drawing at Heatherley's School of Fine Art, Chelsea and The Art Academy, London.</p>
-    <p>Banji is married to Ruth, who is a project manager and transformational coach; they have two kids Joshua and Kezia, who are both twelve and eight years respectively.</p>
-</article>
+    <article>
+        <h2>Adebanji Alade</h2>
+        <p>Banji started off drawing when he was six years old, something he has always loved to do. This passion to sketch and draw has blossomed into a full professional career as a painter of people and places. He is now a member of the Royal Institute of Oil Painters, The Chelsea Art Society and The Guild of Fine Art, Nigeria. He has made many appearances on TV, sketching and painting portraits of famous people on The One Show, BBC One. He is also a London Correspondent with Urban Sketchers Worldwide. Adebanji teaches Painting and Drawing at Heatherley's School of Fine Art, Chelsea and The Art Academy, London.</p>
+        <p>Banji is married to Ruth, who is a project manager and transformational coach; they have two kids Joshua and Kezia, who are both twelve and eight years respectively.</p>
+    </article>
 
-<article>
-    <h2>Claire Fulford</h2>
-    <p>Based in Staffordshire, Artist and Illustrator Claire originally trained as an Animator. Years after, her fascination with cartoons and passion for art combined to form her business Cartoonarts. With the help of The Princes Trust Claire started work on a collection of artworks which she showcased in exhibitions and events around the country. Her works have since evolved to working on a whole range of creative projects such as freelance illustration, graphic design, pet commissions, product design and of course children's books.</p>
-    <p>Her creativity now also extends to finding new and exciting ways to amuse her energetic and passionate 3 year old daughter.</p>
-</article>
+    <article>
+        <h2>Claire Fulford</h2>
+        <p>Based in Staffordshire, Artist and Illustrator Claire originally trained as an Animator. Years after, her fascination with cartoons and passion for art combined to form her business Cartoonarts. With the help of The Princes Trust Claire started work on a collection of artworks which she showcased in exhibitions and events around the country. Her works have since evolved to working on a whole range of creative projects such as freelance illustration, graphic design, pet commissions, product design and of course children's books.</p>
+        <p>Her creativity now also extends to finding new and exciting ways to amuse her energetic and passionate 3 year old daughter.</p>
+    </article>
 
-<article>
-    <h2>Nick Bergquist</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-    <p>And he has cats</p>
-</article>
+    <article>
+        <h2>Nick Bergquist</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>And he has cats</p>
+    </article>
 
-<article>
-    <h2>Christopher Clarke</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-    <p>And he has dogs</p>
-</article>
+    <article>
+        <h2>Christopher Clarke</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>And he has dogs</p>
+    </article>
 
-<article>
-    <h2>Sharmina Karim</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-    <p>And something else</p>
-</article>
+    <article>
+        <h2>Sharmina Karim</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>And something else</p>
+    </article>
+</div>
